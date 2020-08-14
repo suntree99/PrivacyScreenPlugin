ba4d6ca1e738ed68e9a1dc91bd1c04ab8d41d043
Truewind <labs@truewind.pt>
2020-08-14 17:00:22 +0100

    Update PrivacyScreenPlugin.java

--- src/android/PrivacyScreenPlugin.java
+++ src/android/PrivacyScreenPlugin.java
@@ -7,37 +7,29 @@
 package org.devgeeks.privacyscreen;
 
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
 
-import android.app.Activity;
 import android.view.Window;
 import android.view.WindowManager;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.content.Context;
-import android.os.Bundle;
 
 /**
  * This class sets the FLAG_SECURE flag on the window to make the app
  *  private when shown in the task switcher
  */
 public class PrivacyScreenPlugin extends CordovaPlugin {
+
   @Override
-  protected void onPause() {
-    super.onPause();
-    Activity activity = this.cordova.getActivity();
-    activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,WindowManager.LayoutParams.FLAG_SECURE);
+  public void onPause(boolean multitasking) {
+    Window window = this.cordova.getActivity().getWindow();
+    window.addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    super.onPause(multitasking);
   }
 
   @Override
-  protected void onResume() {
-    super.onResume();
-    Activity activity = this.cordova.getActivity();
-    activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+  public void onResume(boolean multitasking) {
+    Window window = this.cordova.getActivity().getWindow();
+    window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    super.onResume(multitasking);
   }
 }
