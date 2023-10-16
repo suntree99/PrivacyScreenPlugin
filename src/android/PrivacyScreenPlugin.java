467c7100e963ace01606ed6bc3342fb8a9647aff
Budi Darmawan <63173420+suntree99@users.noreply.github.com>
2023-10-16 15:58:14 +0700

    Update PrivacyScreenPlugin.java
    Enabled initialize and added dependencies

--- src/android/PrivacyScreenPlugin.java
+++ src/android/PrivacyScreenPlugin.java
@@ -10,8 +10,18 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
 
+import org.apache.cordova.CallbackContext;
+import android.app.Activity;
+
 import android.view.Window;
 import android.view.WindowManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.os.Bundle;
 
 /**
  * This class sets the FLAG_SECURE flag on the window to make the app
@@ -19,12 +29,12 @@ import android.view.WindowManager;
  */
 public class PrivacyScreenPlugin extends CordovaPlugin {
 
-  // @Override
-  // public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-  //   super.initialize(cordova, webView);
-  //   Activity activity = this.cordova.getActivity();
-  //   activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
-  // }
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+    Activity activity = this.cordova.getActivity();
+    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+  }
 
   @Override
   public void onPause(boolean multitasking) {
