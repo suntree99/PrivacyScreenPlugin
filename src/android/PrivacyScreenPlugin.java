73c990ef85019bb80cde06d63bc4b0e942b6e42f
Budi Darmawan <63173420+suntree99@users.noreply.github.com>
2023-10-16 15:25:20 +0700

    Update PrivacyScreenPlugin.java
    Disambled initialize

--- src/android/PrivacyScreenPlugin.java
+++ src/android/PrivacyScreenPlugin.java
@@ -19,12 +19,12 @@ import android.view.WindowManager;
  */
 public class PrivacyScreenPlugin extends CordovaPlugin {
 
-  @Override
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
-    Activity activity = this.cordova.getActivity();
-    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
-  }
+  // @Override
+  // public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+  //   super.initialize(cordova, webView);
+  //   Activity activity = this.cordova.getActivity();
+  //   activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+  // }
 
   @Override
   public void onPause(boolean multitasking) {
